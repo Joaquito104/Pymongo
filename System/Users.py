8608ea56211866692f4da30a24673a0ddbf40bf9
Joaquito104 <joaquin.rivera14@inacapmail.cl>
2025-04-27 01:17:27 -0400

    Crud completado

--- System/Users.py
+++ System/Users.py
@@ -34,7 +34,7 @@ def ordenSalmon ():
         ganancias.insert_one({'Salmon':'Atlantico', 'Costo':costo1, 'Ganancia': totalKilo1 - costo1}).inserted_id
        
 
-    if salmon == 2:
+    if salmon == '2':
 
         print('El precio del Salmon Nordico por kilo es: $7000 ')
         kilo2= int(input('Cuantos kilos necesita?: '))
@@ -44,7 +44,7 @@ def ordenSalmon ():
         ventas.insert_one({'Salmon':'Nordico', 'Monto':totalKilo2}).inserted_id
         ganancias.insert_one({'Salmon':'Nordico', 'Costo':costo2, 'Ganancia': totalKilo2 - costo2}).inserted_id
         
-    if salmon == 3:
+    if salmon == '3':
         
         print('El precio del Salmon Pacifico por kilo es: $3000') 
         kilo3= int(input('Cuantos kilos necesita?: '))
@@ -56,13 +56,10 @@ def ordenSalmon ():
         ganancias.insert_one({'Salmon':'Pacifico', 'Costo':costo3, 'Ganancia': totalKilo3 - costo3}).inserted_id
 
     else:
-        print('ERROR. Numero ingresado invalido. Orden cancelada')
+        print('Error 1. Numero ingresado invalido. Orden cancelada')
 
 
 #Usuarios
-
-input
-
 def usuario():
     ingreso = input('Para iniciar como administrador ingrese 1. Para iniciar como vendedor ingrese 2 ')
     if ingreso == '1':
@@ -95,6 +92,23 @@ def usuario():
 
             editar = input('Porfavor ingrese numero correspondinte a las opciones: ')
             
+            if editar == '1':
+                nuevoStock1 = input('Ingrese el numero con el cual se modificara el stock: ')       #Crud : Update
+                nuevoStock1 = int
+                stock.update_one({"Salmon":'Atlantico'},{"$set":{"stock":nuevoStock1}})
+
+            if editar == '2':
+                nuevoStock2 = input('Ingrese el numero con el cual se modificara el stock: ')
+                nuevoStock2 = int
+                stock.update_one({"Salmon":'Atlantico'},{"$set":{"stock":nuevoStock2}})
+
+            if editar == '3':
+                nuevoStock3 = input('Ingrese el numero con el cual se modificara el stock: ')
+                nuevoStock3 = int
+                stock.update_one({"Salmon":'Atlantico'},{"$set":{"stock":nuevoStock3}})
+
+            else: 
+                print('Error. Cancelando accion')          
 
         
     elif ingreso == '2':
